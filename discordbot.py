e98b223f4b0f8fd13f1c7a1e4062d4f7e9599410
hacchuu0119 <masashi.hachuda@gmo.media>
2018-09-08 00:01:53 +0900

    first bot

---
+++ discordbot.py
@@ -0,0 +1,19 @@
+import discord # インストールした discord.py
+
+client = discord.Client() # 接続に使用するオブジェクト
+
+# 起動時に通知してくれる処理
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+# 「/neko」と発言したら「にゃーん」が返る処理
+@client.event
+async def on_message(message):
+    if message.content.startswith('/neko'):
+        reply = 'にゃーん'
+        await client.send_message(message.channel, reply)
+
+# botの接続と起動
+# （tokenにはbotアカウントのアクセストークンを入れてください）
+client.run('NDg3NjEwODY5MTE1NzgxMTIx.DnQLQw.M0Fg5EOxeDsWvOtda7dq2ebQAgQ')
